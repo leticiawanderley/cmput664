c34dad4f91332b775f3673eebfcf430988850cda
Leticia Wanderley <leticiafwanderley@gmail.com>
2019-10-11 14:25:07 -0600

    Using error length and dependency graph

--- parser.py
+++ parser.py
@@ -8,6 +8,8 @@ def extract_data(nlp, xml_file):
   data = {}
   root = etree.parse(xml_file).getroot()
   errors = []
+  total = 0
+  correctionless = 0
   for header in headers:
     for h in root.iter(header):
       if h.text:
@@ -17,8 +19,11 @@ def extract_data(nlp, xml_file):
   answers = ['answer1', 'answer2']
   for answer in answers:
     for a in root.iter(answer):
-      errors += extract_errors(nlp, a)
-  return data, errors
+      e, t, c = extract_errors(nlp, a)
+      errors += e
+      total += t
+      correctionless += c
+  return data, errors, total, correctionless
 
 def replace_non_alphanumeric(text):
   characters = ['-', '\"', ',', '(', ')', ':', ';', '!', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '?', '/', '#', '£', '%', '$', '*', '+', '&', '|', '.', '=']
@@ -30,7 +35,8 @@ def extract_errors(nlp, answer):
   headers = ['exam_score']
   errors_sentences = []
   exam_score = None
-
+  total_number_of_errors = 0
+  number_of_correctionless_errors = 0
   for header in headers:
     for h in answer.iter(header):
       exam_score = h.text
@@ -40,6 +46,8 @@ def extract_errors(nlp, answer):
         nodes = sentence_element.xpath('child::node()')
         errors = list(sentence_element.iter('NS'))
         num_errors = len(errors)
+        total_number_of_errors += num_errors
+        count = 0
         while num_errors > 0:
           correct_sentence = ''
           line = ''
@@ -53,7 +61,7 @@ def extract_errors(nlp, answer):
               if node == errors[num_errors]: # if it is the error at the position examined
                 error_type = node.attrib['type']
                 error_of_interest = True
-                error_position = len(tag_sentence(nlp, line)) # recording error position
+                error_position = len(nlp(" ".join(line.split()))) # recording error position
               else:
                 error_of_interest = False
               for child in list(node): # loop through wrong and corrected utterances
@@ -68,21 +76,32 @@ def extract_errors(nlp, answer):
               text = replace_non_alphanumeric(node.text)
               line += text
               correct_sentence += text
+              count += 1
             elif type(node) != etree._Element: # if the node only contains unicode text
               text = replace_non_alphanumeric(node)
               line += text
               correct_sentence += text
           if error_type and 'P' not in error_type and error_position is not None and error_length is not None: # filtering punctuation and nested errors
-            error_bigram = get_pos_ngram(nlp, line, error_position, 2, error_type)
-            correct_bigram = get_pos_ngram(nlp, correct_sentence, error_position, 2)
-            error_trigram = get_pos_ngram(nlp, line, error_position, 3, error_type)
-            correct_trigram = get_pos_ngram(nlp, correct_sentence, error_position, 3)
-            error_pos = get_pos_ngram(nlp, line, error_position, 1, error_type)
-            correct_pos = get_pos_ngram(nlp, correct_sentence, error_position, 1)
-            error_pos_2 = get_pos_ngram(nlp, line, error_position + 1, 1)
-            correct_pos_2 = get_pos_ngram(nlp, correct_sentence, error_position + 1, 1)
-            error_pos_3 = get_pos_ngram(nlp, line, error_position + 2, 1)
-            correct_pos_3 = get_pos_ngram(nlp, correct_sentence, error_position + 2, 1)
+            line = " ".join(line.split())
+            correct_sentence = " ".join(correct_sentence.split())
+            error_bigram, e_bi_dep = get_pos_ngram(nlp, line, error_position, 2, error_type)
+            correct_bigram, c_bi_dep  = get_pos_ngram(nlp, correct_sentence, error_position, 2)
+            error_trigram, e_tri_dep = get_pos_ngram(nlp, line, error_position, 3, error_type)
+            correct_trigram, c_tri_dep = get_pos_ngram(nlp, correct_sentence, error_position, 3)
+            error_pos, e_dep = get_pos_ngram(nlp, line, error_position, 1, error_type)
+            correct_pos, c_dep = get_pos_ngram(nlp, correct_sentence, error_position, 1)
+            error_pos_2, e_dep_2 = get_pos_ngram(nlp, line, error_position + 1, 1)
+            correct_pos_2, c_dep_2 = get_pos_ngram(nlp, correct_sentence, error_position + 1, 1)
+            error_pos_3, e_dep_3 = get_pos_ngram(nlp, line, error_position + 2, 1)
+            correct_pos_3, c_dep_3 = get_pos_ngram(nlp, correct_sentence, error_position + 2, 1)
+            first_first_pos, first_second_pos, first_third_pos, \
+              second_first_pos, second_second_pos, second_third_pos, \
+                third_first_pos, third_second_pos, third_third_pos = compare_tags(error_pos, error_pos_2, error_pos_3,
+                                                                                  correct_pos, correct_pos_2, correct_pos_3)
+            first_first_dep, first_second_dep, first_third_dep, \
+              second_first_dep, second_second_dep, second_third_dep, \
+                third_first_dep, third_second_dep, third_third_dep = compare_tags(e_dep, e_dep_2, e_dep_3,
+                                                                                  c_dep, c_dep_2, c_dep_3)
             errors_sentences.append({'line': line, 'correct_sentence': correct_sentence, 'exam_score': exam_score,
               'error_type': error_type, 'error_position': error_position,
               'error_trigram': error_trigram, 'correct_trigram': correct_trigram,
@@ -90,43 +109,78 @@ def extract_errors(nlp, answer):
               'error_pos': error_pos, 'correct_pos': correct_pos,
               'error_pos_2': error_pos_2, 'correct_pos_2': correct_pos_2,
               'error_pos_3': error_pos_3, 'correct_pos_3': correct_pos_3,
+              'e_tri_dep': e_tri_dep, 'c_tri_dep': c_tri_dep,
+              'e_bi_dep': e_bi_dep, 'c_bi_dep': c_bi_dep,
+              'e_dep': e_dep, 'c_dep': c_dep,
+              'e_dep_2': e_dep_2, 'c_dep_2': c_dep_2,
+              'e_dep_3': e_dep_3, 'c_dep_3': c_dep_3,
+              'first_first_pos': first_first_pos, 'first_second_pos': first_second_pos, 'first_third_pos': first_third_pos,
+              'second_first_pos': second_first_pos, 'second_second_pos': second_second_pos, 'second_third_pos': second_third_pos,
+              'third_first_pos': third_first_pos, 'third_second_pos': third_second_pos, 'third_third_pos': third_third_pos,
+              'first_first_dep': first_first_dep, 'first_second_dep': first_second_dep, 'first_third_dep': first_third_dep,
+              'second_first_dep': second_first_dep, 'second_second_dep': second_second_dep, 'second_third_dep': second_third_dep,
+              'third_first_dep': third_first_dep, 'third_second_dep': third_second_dep, 'third_third_dep': third_third_dep,
               'error_length': error_length})
-  return errors_sentences
+        number_of_correctionless_errors += count/len(errors)
+  return errors_sentences, total_number_of_errors, number_of_correctionless_errors
 
 def tag_sentence(nlp, sentence):
   if sentence.isupper():
     sentence = sentence.lower()
-  doc = nlp(sentence)
   tags = []
-  for token in doc:
-    if token.tag_ != '_SP':
-      tags.append(token.tag_)
-  return tags
+  deps = []
+  for sent in nlp.pipe([sentence], disable=["ner", "textcat"]):
+    for token in sent:
+      if token.tag_ != '_SP':
+        tags.append(token.tag_)
+        deps.append(token.dep_)
+  return tags, deps
 
 def get_pos_ngram(nlp, sentence, index, n, error_type=None):
-  tags = tag_sentence(nlp, sentence)
+  tags, deps = tag_sentence(nlp, sentence)
   pos_tags = ''
+  dep_tags = ''
   if error_type and 'M' in error_type: # adding a blank tag for 'missing' error types
     n -= 1
     pos_tags = '_ '
+    dep_tags = '_ '
   tags.extend(['*'] * (n + 2))
+  deps.extend(['*'] * (n + 2))
   for i in range(index, index + n):
     pos_tags += tags[i] + " "
-  return pos_tags
+    dep_tags += deps[i] + " "
+  return pos_tags, dep_tags
+
+def compare_tags(first_incorrect, second_incorrect, third_incorrect,
+                  first_correct, second_correct, third_correct):
+  return first_incorrect == first_correct, first_incorrect == second_correct, first_incorrect == third_correct,\
+          second_incorrect == first_correct, second_incorrect == second_correct, second_incorrect == third_correct,\
+          third_incorrect == first_correct, third_incorrect == second_correct, third_incorrect == third_correct
 
 def main():
   nlp = spacy.load('en_core_web_lg')
   structure = {'head': [], 'language': [], 'score': [], 'exam_score': [], 'line': [], 'error_position': [], 'error_type': [],
     'correct_sentence': [], 'error_trigram': [], 'correct_trigram': [], 'error_bigram': [], 'correct_bigram': [], 'error_pos': [], 'correct_pos': [],
-    'correct_pos_2': [], 'error_pos_2': [], 'correct_pos_3': [], 'error_pos_3': [], 'error_length': []}
+    'correct_pos_2': [], 'error_pos_2': [], 'correct_pos_3': [], 'error_pos_3': [], 'error_length': [],  'e_tri_dep': [], 'c_tri_dep': [],
+    'e_bi_dep': [], 'c_bi_dep': [], 'e_dep': [], 'c_dep': [], 'e_dep_2': [], 'c_dep_2': [], 'e_dep_3': [], 'c_dep_3': [],
+    'first_first_pos': [], 'first_second_pos': [], 'first_third_pos': [],
+    'second_first_pos': [], 'second_second_pos': [], 'second_third_pos': [],
+    'third_first_pos': [], 'third_second_pos': [], 'third_third_pos': [],
+    'first_first_dep': [], 'first_second_dep': [], 'first_third_dep': [],
+    'second_first_dep': [], 'second_second_dep': [], 'second_third_dep': [],
+    'third_first_dep': [], 'third_second_dep': [], 'third_third_dep': []}
   languages = {}
   dataset = './fce-released-dataset/dataset/'
   directories = listdir(dataset)
+  t = 0
+  c = 0
   for directory in directories:
     if path.isdir(dataset + directory):
       files = listdir(dataset + directory)
       for file in files:
-        data, errors = extract_data(nlp, dataset + directory + '/' + file)
+        data, errors, total, correctionless = extract_data(nlp, dataset + directory + '/' + file)
+        t += total
+        c += correctionless
         if data['language'] not in languages.keys():
           languages[data['language']] = 1
         else:
@@ -150,19 +204,52 @@ def main():
           structure['correct_pos_2'].append(error['correct_pos_2'])
           structure['error_pos_3'].append(error['error_pos_3'])
           structure['correct_pos_3'].append(error['correct_pos_3'])
+          structure['e_tri_dep'].append(error['e_tri_dep'])
+          structure['c_tri_dep'].append(error['c_tri_dep'])
+          structure['e_bi_dep'].append(error['e_bi_dep'])
+          structure['c_bi_dep'].append(error['c_bi_dep'])
+          structure['e_dep'].append(error['e_dep'])
+          structure['c_dep'].append(error['c_dep'])
+          structure['e_dep_2'].append(error['e_dep_2'])
+          structure['c_dep_2'].append(error['c_dep_2'])
+          structure['e_dep_3'].append(error['e_dep_3'])
+          structure['c_dep_3'].append(error['c_dep_3'])
+          
+
+          structure['first_first_pos'].append(error['first_first_pos'])
+          structure['first_second_pos'].append(error['first_second_pos'])
+          structure['first_third_pos'].append(error['first_third_pos'])
+          structure['second_first_pos'].append(error['second_first_pos'])
+          structure['second_second_pos'].append(error['second_second_pos'])
+          structure['second_third_pos'].append(error['second_third_pos'])
+          structure['third_first_pos'].append(error['third_first_pos'])
+          structure['third_second_pos'].append(error['third_second_pos'])
+          structure['third_third_pos'].append(error['third_third_pos'])
+          structure['first_first_dep'].append(error['first_first_dep'])
+
+          structure['first_second_dep'].append(error['first_second_dep'])
+          structure['first_third_dep'].append(error['first_third_dep'])
+          structure['second_first_dep'].append(error['second_first_dep'])
+          structure['second_second_dep'].append(error['second_second_dep'])
+          structure['second_third_dep'].append(error['second_third_dep'])
+          structure['third_first_dep'].append(error['third_first_dep'])
+          structure['third_second_dep'].append(error['third_second_dep'])
+          structure['third_third_dep'].append(error['third_third_dep'])
+
           structure['error_length'].append(error['error_length'])
   df = pd.DataFrame(data=structure)
   df.to_csv(r'./dataframe.csv')
-  print(languages)
+  print(languages, t, c)
 
 def test():
   nlp = spacy.load('en_core_web_lg')
   test1 = '/Users/leticiawanderley/Documents/CMPUT 664/project/fce-released-dataset/dataset/0100_2000_12/doc209.xml'
   test = './fce-released-dataset/dataset/0100_2000_6/doc730.xml'
   #test = './fce-released-dataset/dataset/0102_2000_6/doc2402.xml'
-  data, errors = extract_data(nlp, test1)
+  data, errors, total, correctionless = extract_data(nlp, test1)
+  print(total, correctionless)
   for e in errors:
-    if e['error_type'] == 'RV':
+    if e['error_type'] == 'ID':
       print(e)
 
 if __name__ == "__main__":
